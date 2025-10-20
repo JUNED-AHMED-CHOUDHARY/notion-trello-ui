a82e0b2a453151fe24410db023b4f1d49473a089
Juned Ahmed Choudhary <jjahmedchoudhary@gmail.com>
2025-10-20 12:46:58 +0530

    deploymnet check with restric mode its failing or not

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: false, // disable strict mode
+  reactStrictMode: false
 };
 
 export default withSentryConfig(nextConfig, {
